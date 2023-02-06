2693a902f69db0190e167829abc85b8bb882a38f
Celia Sánchez <celiasanchezmartin6@gmail.com>
2023-02-06 18:30:10 +0100

    second commit

---
+++ retoFunciones.js
@@ -0,0 +1,20 @@
+//añadir el reto de funciones del dia 5.
+function suma (num1, num2){
+    console.log(num1 + num2)
+};
+
+function resta (num1, num2){
+    console.log(num1 - num2)
+};
+
+function multip (num1, num2){
+    console.log(num1 * num2)
+};
+
+function div (num1, num2){
+    console.log(num1 / num2)
+};
+//funcion cubo.
+function cubo(num){
+    return num * num;
+}
